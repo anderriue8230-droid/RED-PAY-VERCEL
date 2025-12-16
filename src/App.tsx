eb23666ba6c0cb6993625868f7faca97c64117b4
anderriue8230-droid <anderriue8230@gmail.com>
2025-12-16 22:23:29 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>RED PAY</h1>
+      <p>App is running successfully 🎉</p>
+    </div>
+  );
+}
